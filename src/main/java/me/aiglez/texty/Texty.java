4cf374d6c42bb038d93828a5c9b2a1b92ec37464
AigleDev <eaglezledozo@gmail.com>
2021-03-26 16:59:10 +0100

    Ignore when the event is cancelled, and added a Cooldown system

--- src/main/java/me/aiglez/texty/Texty.java
+++ src/main/java/me/aiglez/texty/Texty.java
@@ -1,5 +1,6 @@
 package me.aiglez.texty;
 
+import com.google.common.base.Preconditions;
 import com.google.common.collect.Maps;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.kyori.adventure.key.Key;
@@ -7,6 +8,7 @@ import net.kyori.adventure.nbt.api.BinaryTagHolder;
 import net.kyori.adventure.platform.bukkit.BukkitAudiences;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.TextReplacementConfig;
 import net.kyori.adventure.text.event.HoverEvent;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextDecoration;
@@ -15,6 +17,7 @@ import net.kyori.adventure.util.Codec;
 import net.minecraft.server.v1_13_R2.MojangsonParser;
 import net.minecraft.server.v1_13_R2.NBTTagCompound;
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.time.DurationFormatUtils;
 import org.bukkit.Material;
 import org.bukkit.craftbukkit.v1_13_R2.inventory.CraftItemStack;
 import org.bukkit.entity.Player;
@@ -26,6 +29,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Map;
 import java.util.UUID;
 
@@ -54,7 +59,7 @@ public final class Texty extends JavaPlugin implements Listener {
             .append(Component.text(" You aren't holding an item.", NamedTextColor.RED))
             .build();
 
-    private Map<UUID, Long> cooldown;
+    private Map<UUID, Instant> cooldown;
     private BukkitAudiences adventure;
 
     @Override
@@ -74,12 +79,26 @@ public final class Texty extends JavaPlugin implements Listener {
     }
 
     @SuppressWarnings("PatternValidation")
-    @EventHandler
+    @EventHandler(ignoreCancelled = true)
     public void onAsyncChat(final AsyncPlayerChatEvent e) {
         final Player player = e.getPlayer();
         final String message = e.getMessage();
 
         if(StringUtils.containsIgnoreCase(message, PLACEHOLDER)) {
+            if (hasCooldown(player)) {
+                adventure.player(player).sendMessage(Component.text()
+                        .append(Component.text("(", NamedTextColor.DARK_GRAY, TextDecoration.BOLD))
+                        .append(Component.text("!", NamedTextColor.RED, TextDecoration.BOLD))
+                        .append(Component.text(")", NamedTextColor.DARK_GRAY, TextDecoration.BOLD))
+                        .append(Component.text(" You must wait ", NamedTextColor.RED)
+                                .append(Component.text(this.getCooldown(player), NamedTextColor.AQUA))
+                                .append(Component.text(" before you can do this again.", NamedTextColor.RED))
+                        )
+                );
+                e.setCancelled(true);
+                return;
+            }
+
             final ItemStack item = player.getInventory().getItemInMainHand();
             if (item.getType() == Material.AIR) {
                 adventure.player(player).sendMessage(ERROR_NOT_HOLDING_ITEM);
@@ -88,10 +107,11 @@ public final class Texty extends JavaPlugin implements Listener {
 
             Component itemDisplayName;
             if (item.hasItemMeta() && item.getItemMeta().hasDisplayName()) {
-                itemDisplayName = LegacyComponentSerializer.legacyAmpersand().deserialize(item.getItemMeta().getDisplayName());
+                itemDisplayName = LegacyComponentSerializer.legacyAmpersand().deserialize(item.getItemMeta().getDisplayName())
+                        .colorIfAbsent(NamedTextColor.WHITE);
             } else {
-                itemDisplayName = Component.text(StringUtils.capitalize(item.getType().name().replaceAll("_", " ")),
-                        NamedTextColor.WHITE);
+                itemDisplayName = Component.text(StringUtils.capitalize(item.getType().name().replaceAll("_", " ")))
+                        .color(NamedTextColor.WHITE);
             }
 
             NBTTagCompound tag = CraftItemStack.asNMSCopy(item).getOrCreateTag();
@@ -126,8 +146,34 @@ public final class Texty extends JavaPlugin implements Listener {
                     );
 
             adventure.all().sendMessage(component);
+            resetCooldown(player);
+
             e.setCancelled(true);
         }
+    }
+
+
+    private boolean hasCooldown(final Player player) {
+        Preconditions.checkNotNull(player, "player may not be null");
+        final Instant instant = this.cooldown.getOrDefault(player.getUniqueId(), null);
+        if(instant == null) {
+            return false;
+        }
+
+        final Duration duration = Duration.between(instant, Instant.now());
+        return duration.toMinutes() < 3;
+    }
+
+    private void resetCooldown(final Player player) {
+        this.cooldown.put(Preconditions.checkNotNull(player, "player may not be null").getUniqueId(), Instant.now());
+    }
+
+    private String getCooldown(final Player player) {
+        Preconditions.checkNotNull(player, "player may not be null");
+        final Instant instant = this.cooldown.get(player.getUniqueId());
+        final Duration duration = Duration.between(instant, Instant.now());
+
+        return DurationFormatUtils.formatDuration(duration.toMillis(), "mm:ss");
     }
 
     private BinaryTagHolder asBinaryTag(final NBTTagCompound tag) {
